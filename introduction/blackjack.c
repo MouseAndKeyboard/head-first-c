56b4c5a33868b7ee1db96f0630c0865532a7342b
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-01 22:30:05 +0800

    Switch statement

--- introduction/blackjack.c
+++ introduction/blackjack.c
@@ -11,17 +11,19 @@ int main() {
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
   int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
-    val = 11;
-  } else {
-    val = atoi(card_name);
+  switch(card_name[0]){
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    default:
+      val = atoi(card_name);
   }
+
   printf("The card you selected, %s, has the value: %i\n", card_name, val);
 
   if ((val > 2) && (val < 7))

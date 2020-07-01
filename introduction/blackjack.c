1b603d7d5cd12f4504ba241f1e7abe10e82ae546
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-01 23:06:38 +0800

    error checking

--- introduction/blackjack.c
+++ introduction/blackjack.c
@@ -8,28 +8,41 @@
 
 int main() {
   char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-  int val = 0;
-  switch(card_name[0]){
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    default:
-      val = atoi(card_name);
+  int count = 0;
+  while (card_name[0] != 'X') {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch(card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if (val < 1 || val > 10) {
+          printf("Invalid card number: %i\n", val);
+
+          continue;
+        }
+    }
+
+    printf("The card you selected, %s, has the value: %i\n", card_name, val);
+
+    if ((val > 2) && (val < 7)) {
+      puts("Count has gone up");
+      count++;
+    } else if (val == 10) {
+      puts("Count has gone down");
+      count--;
+    }
+    printf("Current count: %i\n", count);
   }
-
-  printf("The card you selected, %s, has the value: %i\n", card_name, val);
-
-  if ((val > 2) && (val < 7))
-    puts("Count has gone up");
-  else if (val == 10)
-    puts("Count has gone down");
-
   return 0;
 }

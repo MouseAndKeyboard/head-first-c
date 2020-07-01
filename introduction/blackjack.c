ca8c9ab851ff239578f77512a1b855f1f986e354
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-01 22:03:38 +0800

    initial commit

---
+++ introduction/blackjack.c
@@ -0,0 +1,28 @@
+/*
+ * Program to evaluate face values.
+ * license and copyright information
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char card_name[3];
+  puts("Enter the card_name: ");
+  scanf("%2s", card_name);
+  int val = 0;
+  if (card_name[0] == 'K') {
+    val = 10;
+  } else if (card_name[0] == 'Q') {
+    val = 10;
+  } else if (card_name[0] == 'J') {
+    val = 10;
+  } else if (card_name[0] == 'A') {
+    val = 11;
+  } else {
+    val = atoi(card_name);
+  }
+  printf("The card you selected, %s, has the value: %i\n", card_name, val);
+
+  return 0;
+}

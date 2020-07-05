9aee0241d34f0cd1ca994f83ff350a9724c93d72
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-05 19:25:55 +0800

    forking processes!

--- breaking_boundaries/multi_rss.c
+++ breaking_boundaries/multi_rss.c
@@ -8,16 +8,25 @@ int main(int argc, char* argv[]) {
     char *feeds[] = { "https://www.lesswrong.com/feed.xml?view=community-rss&karmaThreshold=30",
                       "https://overreacted.io/rss.xml"};
 
-    int times = 3;
+    int times = 2;
     char *phrase = argv[1];
     for (int i = 0; i < times; i++) {
         char var[255];
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
-        if (execle("/usr/bin/python2", "usr/bin/python2", "./rssgossip.py", phrase, NULL, vars) == -1) {
-            fprintf(stderr, "Can't run script: %s.\n", strerror(errno));
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            fprintf(stderr, "Couldn't fork process: %s.\n", strerror(errno));
             return 1;
         }
-    }
 
+        if (!pid) {
+            if (execle("/usr/bin/python2", "usr/bin/python2", "./rssgossip.py", phrase, NULL, vars) == -1) {
+                fprintf(stderr, "Can't run script: %s.\n", strerror(errno));
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
